6e0409624615a979104e6ad555a9560adb0e9739
Mayra Cabrera <mcabrera1087@gmail.com>
2016-08-25 22:47:06 -0500

    Update first answer

--- lib/anchors.rb
+++ lib/anchors.rb
@@ -3,7 +3,7 @@ class Anchors
   # Returns a regex that matches a string that has at least one line that has
   # exactly 1 word in it. It may have other lines that don't match this regex.
   def matching_at_least_one_line_that_has_exactly_one_word_in_it
-    /\A(\b)+\b*\z/
+    /^\w+$/
   end
 
   # Returns a regex that matches a string that starts with "hello".
